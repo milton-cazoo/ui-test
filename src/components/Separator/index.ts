67a12432f8035ba41d49a43b23739bf544b647c1
milton-cazoo <ext_milton.junior@cazoo.co.uk>
2022-09-08 14:18:50 +0100

    add git repo information

--- src/components/Separator/index.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Separator = styled.hr`
-  width: 100%;
-  height: 2px;
-  background-color: #666;
-`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,3 @@
 export { Button } from "./components/Button";
-export { Separator } from "./components/Separator";
 export { Input } from "./components/Input";
-export { Radio } from "./components/Radio";
 export { CheckBox } from "./components/Checkbox";
-export { Modal } from "./components/Modal";
